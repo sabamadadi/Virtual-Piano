6d23bb841afa87b57ba742574f54a019fb5d6e7b
Saba Madadi <sabamadadi9@gmail.com>
2023-08-16 15:49:33 +0330

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <Windows.h> // for Windows platform only
+
+// Function to play a piano key sound
+void PlaySound(int frequency, int duration) {
+    if (frequency == 0) {
+        Sleep(duration);
+    } else {
+        Beep(frequency, duration);
+    }
+}
+
+int main() {
+    // Define piano key frequencies
+    const int numKeys = 7;
+    const int frequencies[numKeys] = {262, 294, 330, 349, 392, 440, 494};  // C4, D4, E4, F4, G4, A4, B4
+
+    // Define piano key labels
+    const char keyLabels[numKeys] = {'A', 'S', 'D', 'F', 'G', 'H', 'J'};
+
+    // Print piano key labels
+    std::cout << "Virtual Piano\n";
+    std::cout << "Press the corresponding keys to play the piano:\n";
+    for (int i = 0; i < numKeys; ++i) {
+        std::cout << keyLabels[i] << " ";
+    }
+    std::cout << "\n\n";
+
+    // Play piano keys until program termination
+    while (true) {
+        if (GetAsyncKeyState(VK_ESCAPE)) {  // Exit program if ESC key is pressed
+            break;
+        }
+
+        for (int i = 0; i < numKeys; ++i) {
+            if (GetAsyncKeyState(keyLabels[i])) {
+                PlaySound(frequencies[i], 300);  // Play sound for 300 milliseconds
+            }
+        }
+    }
+
+    return 0;
+}
